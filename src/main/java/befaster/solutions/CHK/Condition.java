28b833084c63bcf2dd30d7d4791e383b83b334f2
sbx_user1051 <sbx_user1051@169.254.75.45>
2019-10-13 19:43:05 +0000

    Sun Oct 13 19:43:05 UTC 2019

---
+++ src/main/java/befaster/solutions/CHK/Condition.java
@@ -0,0 +1,4 @@
+package befaster.solutions.CHK;
+
+public class Condition {
+}
---
+++ src/main/java/befaster/solutions/CHK/Product.java
@@ -0,0 +1,13 @@
+package befaster.solutions.CHK;
+
+import java.util.Map;
+
+public class Product {
+    
+    private String name;
+    
+    private int price;
+    
+    // Combo quantity -> price
+    private Map<Integer, Integer> specialConditions;
+}
