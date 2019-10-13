714edcf01e901e67e1fe517d47ec7fb5b3f542b2
sbx_user1051 <sbx_user1051@169.254.75.45>
2019-10-13 18:34:37 +0000

    Sun Oct 13 18:34:37 UTC 2019

--- src/main/java/befaster/solutions/CHL/CheckliteSolution.java
+++ src/main/java/befaster/solutions/CHL/CheckliteSolution.java
@@ -1,9 +1,16 @@
 package befaster.solutions.CHL;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CheckliteSolution {
     public Integer checklite(String skus) {
         if (isValid(skus)) {
+            Map<String, Integer> quantities = new HashMap<>();
+            String skusUpper = skus.toUpperCase();
+            skusUpper.chars().forEach(c -> {
 
+            });
         }
         return -1;
     }
@@ -11,9 +18,10 @@ public class CheckliteSolution {
     private boolean isValid(String skus) {
         return skus != null
                 && !skus.isEmpty()
-                && skus.toUpperCase().replaceAll("[A-D]", "").length() == skus.length();
+                && skus.toUpperCase().replaceAll("[A-D]+", "").length() == skus.length();
     }
 }
+
 
 
 
