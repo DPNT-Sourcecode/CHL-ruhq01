d4c2fb131debbe5fd58065577b999a8a0ce3bf12
sbx_user1051 <sbx_user1051@169.254.75.45>
2019-10-13 18:58:37 +0000

    Sun Oct 13 18:58:37 UTC 2019

--- src/test/java/befaster/solutions/CHL/CheckliteSolutionTest.java
+++ src/test/java/befaster/solutions/CHL/CheckliteSolutionTest.java
@@ -1,4 +1,44 @@
 package befaster.solutions.CHL;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 public class CheckliteSolutionTest {
+
+    private final static int DEFAULT_VALUE = -1;
+
+    private CheckliteSolution solution;
+
+    @Before
+    public void setup() {
+        solution = new CheckliteSolution();
+    }
+
+    @Test
+    public void should_returnDefaultValue_when_invalidInput() {
+        String input = "ABC45";
+        assertThat(solution.checklite(input), equalTo(DEFAULT_VALUE));
+    }
+
+    @Test
+    public void should_returnDefaultValue_when_inputNull() {
+        String input = null;
+        assertThat(solution.checklite(input), equalTo(DEFAULT_VALUE));
+    }
+
+    @Test
+    public void should_returnDefaultValue_when_inputIsEmptyList() {
+        String input = "";
+        assertThat(solution.checklite(input), equalTo(DEFAULT_VALUE));
+    }
+
+    @Test
+    public void should_returnCorrectSum_when_validInput() {
+        String input = "AAAABBBCD";
+        assertThat(solution.checklite(input), equalTo(290));
+    }
 }
+
