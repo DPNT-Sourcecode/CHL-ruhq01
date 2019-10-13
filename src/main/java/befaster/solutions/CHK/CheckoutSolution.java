9339698dc51d47b91cd46e1f87ffe29839a7f14c
sbx_user1051 <sbx_user1051@169.254.75.45>
2019-10-13 19:28:05 +0000

    Sun Oct 13 19:28:05 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,7 +1,9 @@
 package befaster.solutions.CHK;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class CheckoutSolution {
     public Integer checkout(String skus) {
@@ -33,24 +35,8 @@ public class CheckoutSolution {
     }
 
     private Map<String, Integer> groupSkus(String skus) {
-        String skusUpper = skus.toUpperCase();
-        Map<String, Integer> quantities = new HashMap<String, Integer>() {{
-            put("A", 0);
-            put("B", 0);
-            put("C", 0);
-            put("D", 0);
-        }};
-        skusUpper.chars().forEach(c -> {
-            if (c == 'C') {
-                quantities.put("C", quantities.get("C") + 1);
-            } else if (c == 'D') {
-                quantities.put("D", quantities.get("D") + 1);
-            } else if (c == 'A') {
-                quantities.put("A", quantities.get("A") + 1);
-            } else {
-                quantities.put("B", quantities.get("B") + 1);
-            }
-        });
+        Arrays.stream(skus.split("")).collect(Collectors.groupingBy(c -> ))
+
         return quantities;
     }
 
@@ -59,5 +45,6 @@ public class CheckoutSolution {
                 && skus.replaceAll("[A-D]+", "").isEmpty();
     }
 }
+
 
 
