8f6665e21871cb7224f6de1d74f59da449cf26d2
sbx_user1051 <sbx_user1051@169.254.75.45>
2019-10-13 19:07:37 +0000

    Sun Oct 13 19:07:37 UTC 2019

--- src/main/java/befaster/solutions/CHL/CheckliteSolution.java
+++ src/main/java/befaster/solutions/CHL/CheckliteSolution.java
@@ -29,7 +29,7 @@ public class CheckliteSolution {
             Integer specialPrice,
             Integer comboQuantity
     ) {
-        return ((quantity % comboQuantity) * normalPrice) + ((quantity - (quantity % comboQuantity)) * specialPrice);
+        return ((quantity % comboQuantity) * normalPrice) + (int) Math.floor(quantity / comboQuantity) * specialPrice;
     }
 
     private Map<String, Integer> groupSkus(String skus) {
@@ -60,6 +60,7 @@ public class CheckliteSolution {
                 && skus.toUpperCase().replaceAll("[A-D]+", "").isEmpty();
     }
 }
+
 
 
 
